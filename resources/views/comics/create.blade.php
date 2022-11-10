d0ebc9906520f8a60b2d45721aca1b51e99c1cc4
antoinde <antonello.indelicato98@gmail.com>
2022-11-10 19:53:49 +0100

    Added body of the page

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,5 +1,13 @@
-
-@if($errors->any()) <!-- VALIDATION -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @if($errors->any()) <!-- VALIDATION -->
     <div class="alert alert-danger">
         <ul>
             @foreach($errors->all() as $error)
@@ -44,3 +52,5 @@
         <input type="submit" value="Send">
     </div>
 </form>
+</body>
+</html>
