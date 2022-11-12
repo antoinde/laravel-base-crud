45fd7e0c0b38d9844dc77ecad0e1911dad91dc09
antoinde <antonello.indelicato98@gmail.com>
2022-11-12 19:20:23 +0100

    Aggiunte classi di error negli input + skin su css

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -25,31 +25,38 @@
         @csrf
         <div>
             <label for="title">title:</label>
-            <input type="text" name="title" required value="{{ old('title', '') }}">
+            <input type="text" name="title" required value="{{ old('title', '') }}"
+            @error('title') class="is-invalid" @enderror>
         </div>
         <div>
             <label for="description">description:</label>
-            <input type="text" name="description" required value="{{ old('description', '') }}">
+            <input type="text" name="description" required value="{{ old('description', '') }}"
+            @error('description') class="is-invalid" @enderror>
         </div>
         <div>
             <label for="thumb">thumb link:</label>
-            <input type="url" name="thumb" required value="{{ old('thumb', '') }}">
+            <input type="url" name="thumb" required value="{{ old('thumb', '') }}"
+            @error('thumb') class="is-invalid" @enderror>
         </div>
         <div>
             <label for="price">price:</label>
-            <input type="number" name="price" required value="{{ old('price', '') }}">
+            <input type="number" name="price" required value="{{ old('price', '') }}"
+            @error('price') class="is-invalid" @enderror>
         </div>
         <div>
             <label for="series">series:</label>
-            <input type="text" name="series" required value="{{ old('series', '') }}">
+            <input type="text" name="series" required value="{{ old('series', '') }}"
+            @error('series') class="is-invalid" @enderror>
         </div>
         <div>
             <label for="sale_date">sale_date:</label>
-            <input type="date" name="sale_date" required value="{{ old('sale_date', '') }}">
+            <input type="date" name="sale_date" required value="{{ old('sale_date', '') }}"
+            @error('sale_date') class="is-invalid" @enderror>
         </div>
         <div>
             <label for="type">type:</label>
-            <input type="text" name="type" required value="{{ old('type', '') }}">
+            <input type="text" name="type" required value="{{ old('type', '') }}"
+            @error('type') class="is-invalid" @enderror>
         </div>
         <div>
             <input type="submit" value="Send">
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Edit Page</title>
 </head>
 
@@ -26,32 +28,38 @@
         @method('PUT')
         <div>
             <label for="title">title:</label>
-            <input type="text" name="title" required value="{{ old('title', $comic->title) }}">
+            <input type="text" name="title" required value="{{ old('title', $comic->title) }}"
+            @error('title') class="is-invalid" @enderror>
         </div>
         <div>
             <label for="description">description:</label>
-            <input type="text" name="description" required 
-                value="{{ old('description', $comic->description) }}">
+            <input type="text" name="description" required value="{{ old('description', $comic->description) }}"
+            @error('description') class="is-invalid" @enderror>
         </div>
         <div>
             <label for="thumb">thumb link:</label>
-            <input type="url" name="thumb" required value="{{ old('thumb', $comic->thumb) }}">
+            <input type="url" name="thumb" required value="{{ old('thumb', $comic->thumb) }}"
+            @error('thumb') class="is-invalid" @enderror>
         </div>
         <div>
             <label for="price">price:</label>
-            <input type="number" name="price" required value="{{ old('price', $comic->price) }}">
+            <input type="number" name="price" required value="{{ old('price', $comic->price) }}"
+            @error('price') class="is-invalid" @enderror>
         </div>
         <div>
             <label for="series">series:</label>
-            <input type="text" name="series" required value="{{ old('series', $comic->series) }}">
+            <input type="text" name="series" required value="{{ old('series', $comic->series) }}"
+            @error('series') class="is-invalid" @enderror>
         </div>
         <div>
             <label for="sale_date">sale_date:</label>
-            <input type="date" name="sale_date" required value="{{ old('sale_date', $comic->sale_date) }}">
+            <input type="date" name="sale_date" required value="{{ old('sale_date', $comic->sale_date) }}"
+            @error('sale_date') class="is-invalid" @enderror>
         </div>
         <div>
             <label for="type">type:</label>
-            <input type="text" name="type" required value="{{ old('type', $comic->type) }}">
+            <input type="text" name="type" required value="{{ old('type', $comic->type) }}"
+            @error('type') class="is-invalid" @enderror>
         </div>
         <div>
             <input type="submit" value="Save changes">
